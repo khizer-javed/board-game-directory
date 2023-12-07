212b641fe3cc81cfb5362ba9246fd489177cb004
Khizer Javed Iqbal <khizerjaved25@gmail.com>
2023-12-07 09:27:34 +0000

    front page designed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // Main entry point for application
 
 const express = require('express');
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
@@ -15,56 +15,53 @@ const env = process.env.NODE_ENV || 'development';
 const config = sequelizeConfig[env]
 
 
-// // TODO: application port should come from config file
-const port = config.port
+async function main() {
+  // TODO: application port should come from config file
+  const port = config.port
 
-// // TODO: database connection parameters should come from config file
-// const db = mysql.createConnection({
-// 	host: 'localhost',
-// 	user: 'app',
-// 	password: 'wonderful',
-// 	database: 'miechallenge'})
-
-// db.connect((err) => {
-// 	if (err) {
-// 		throw err;
-// 	}
-// 	console.log('Connected to database');
-// });
-
-
-// global.db = db;
-
-// Create Sequelize instance
-const sequelize = new Sequelize(config.db);
-
-// Test the connection
-(async () => {
+  // TODO: database connection parameters should come from config file
+  const sequelize = new Sequelize(config.db);
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
-})();
 
-app.set('port', port);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }));
+  // const db = mysql.createConnection({
+  // 	host: 'localhost',
+  // 	user: 'app',
+  // 	password: 'wonderful',
+  // 	database: 'miechallenge'})
 
-// If there are static files, make a public directory
-app.use(express.static(path.join(__dirname, 'public')));
+  // db.connect((err) => {
+  // 	if (err) {
+  // 		throw err;
+  // 	}
+  // 	console.log('Connected to database');
+  // });
 
-// Routes
-app.get('/', getHomePage);
-app.get('/add-game', game.getAdd);
-app.post('/add-game', game.postAdd);
-app.get('/edit-game/:id', game.getEdit);
-app.post('/edit-game/:id', game.postEdit);
-app.get('/add-game-session', game_session.getAdd);
-app.post('/add-game-session', game_session.postAdd);
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+  // global.db = db;
+
+  app.set('port', port);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  // If there are static files, make a public directory
+
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // Routes
+  app.get('/', getHomePage);
+  app.get('/add-game', game.getAdd);
+  app.post('/add-game', game.postAdd);
+  app.get('/edit-game/:id', game.getEdit);
+  app.post('/edit-game/:id', game.postEdit);
+  app.get('/add-game-session', game_session.getAdd);
+  app.post('/add-game-session', game_session.postAdd);
+  app.listen(port);
+  console.log('running on port', port);
+}
+main()
--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,21 @@ module.exports = {
 			{ id: 1, name: 'Game 1' },
 			{ id: 2, name: 'Game 2' },
 			{ id: 3, name: 'Game 3' },
-		];
-		res.render('../views/games/index.ejs', {
+			{ id: 4, name: 'Game 4' },
+			{ id: 5, name: 'Game 5' },
+			{ id: 6, name: 'Game 6' },
+			{ id: 7, name: 'Game 7' },
+			{ id: 8, name: 'Game 8' },
+			{ id: 9, name: 'Game 9' },
+			{ id: 10, name: 'Game 10' },
+			{ id: 11, name: 'Game 11' },
+			{ id: 12, name: 'Game 12' },
+			{ id: 13, name: 'Game 13' },
+			{ id: 14, name: 'Game 14' },
+			{ id: 15, name: 'Game 15' },
+		]
+
+		res.render('../views/games-list/index.ejs', {
 			title: 'Board Games | View Games',
 			games
 			// players: result
